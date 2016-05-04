dc111b89e360968e00f4fe422b61f98253d516ed
mark pritchett <markpritchett@hotmail.co.uk>
2016-05-04 20:29:03 +0100

    Added html and coverage reporters for karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,19 @@
 module.exports = function(config) {
   config.set({
     frameworks: ['jasmine'], 
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage', 'html'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
     singleRun: false,
-    concurrency: Infinity
+    concurrency: Infinity,
+    preprocessors: {
+      'src/app/**/!(*.spec|*.po|*.scenario).js': ['coverage']
+    },
+    htmlReporter: {
+      preserveDescribeNesting: true
+    }
   });
 };
